4f58a067b63926e2991d1f0eb9b4ea0d219fdc8a
Juan Echavarria <juanea@gmail.com>
2017-11-17 10:55:34 -0600

    Problem 750A Codeforces

---
+++ A. New Year and Hurry.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "iostream"
+
+using namespace std;
+
+int n, k, b, c, a, i;
+const int contestLast = 240;
+
+int main()
+{
+	scanf("%d%d", &n, &k);
+	b = contestLast - k;
+	for (i = 0; i < n; i++)
+	{
+		a += 5;
+		c += a;
+		if (c > b)
+			break;
+	}
+	printf("%d", i);
+}
