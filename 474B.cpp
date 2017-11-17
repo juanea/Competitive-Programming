e6d3815f8b8585ac809919a0ec9a0c953b05b6d3
Juan Echavarria <juanea@gmail.com>
2017-11-17 17:58:42 -0600

    Read is ok, cont++ has to be reseted

--- 474B.cpp
+++ 474B.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+#include <bits\stdc++.h>
 
 using namespace std;
+int i, j, n, m, arr[100000], arr2[100000], cont;
 
 int main()
 {
-	cout << "Hello World!";
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	scanf("%d", &m);
+	for (i = 0; i < m; i++)
+		scanf("%d", &arr2[i]);
+	while (m--)
+	{
+		for (i = 0; i < arr[j]; i++)
+		{
+			cont++;
+			if (arr2[j] == cont)
+			{
+				printf("%d\n", j+1);
+				break;
+			}
+		}
+		j += 1;
+	}
 }
